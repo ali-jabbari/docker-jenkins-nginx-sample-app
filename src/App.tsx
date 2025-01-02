057fcbf3bb10d0697fc931dae310e88927635a94
ali-jabbari <md.ali.jabb@gmail.com>
2025-01-02 21:21:48 +0330

    first init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Docker, Jenkins, Nginx Sample App</h1>
+      <p className="read-the-docs">
+        This React Project includes a Dockerized environment, continuous
+        integration (CI) and continuous deployment (CD) pipelines with Jenkins,
+        and an Nginx server.
+      </p>
+    </>
+  );
+}
+
+export default App;
