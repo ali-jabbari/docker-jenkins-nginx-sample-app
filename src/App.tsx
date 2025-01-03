40e81e0d64413d0e8181aec0fffd1a2293afb96a
ali-jabbari <md.ali.jabb@gmail.com>
2025-01-03 04:02:44 +0330

    modify Jenkinsfile

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Docker, Jenkins, Nginx Sample App</h1>
+      <h1>Docker, Jenkins Sample App</h1>
       <p className="read-the-docs">
         This React Project includes a Dockerized environment, continuous
-        integration (CI) and continuous deployment (CD) pipelines with Jenkins,
-        and an Nginx server.
+        integration (CI) and continuous deployment (CD) pipelines with Jenkins.
       </p>
     </>
   );
